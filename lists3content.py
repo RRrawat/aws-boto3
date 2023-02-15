ac8234ea881211d80fc875930a23ad955ad79e95
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-15 17:34:09 +0530

    Update lists3content.py

--- lists3content.py
+++ lists3content.py
@@ -13,3 +13,12 @@ response = s3.list_objects_v2(Bucket=bucket_name)
 for obj in response['Contents']:
     print(obj['Key'])
 """
+This script first initializes an S3 client using the Boto3 library. It then sets the name of the bucket that you want to list the contents of. 
+The script then uses the list_objects_v2 method to retrieve a list of all the objects in the bucket. Finally, the script iterates over the 
+objects in the response and prints their names using the Key attribute.
+
+Note that if there are a large number of objects in the bucket, you may need to use pagination to retrieve all the objects. 
+You can do this by setting the MaxKeys parameter in the list_objects_v2 method to the maximum number of objects you want to 
+retrieve per page, and then using the NextContinuationToken value from the response to retrieve the next page of results.
+
+"""
