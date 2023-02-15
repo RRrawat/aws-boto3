17582fe7cc5de205b70bc40d07a1d631b996768b
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-15 17:34:33 +0530

    Update lists3content.py

--- lists3content.py
+++ lists3content.py
@@ -3,8 +3,8 @@ import boto3
 # Initialize S3 client
 s3 = boto3.client('s3')
 
-# Replace 'my-bucket' with the name of your S3 bucket
-bucket_name = 'my-bucket'
+# Replace 'bucket' with the name of your S3 bucket
+bucket_name = 'bucket'
 
 # List all objects in the bucket
 response = s3.list_objects_v2(Bucket=bucket_name)
