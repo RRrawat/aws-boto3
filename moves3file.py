f1a0265b1b4389970465b4b2aee52ad7c59d013e
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-17 12:42:56 +0530

    Update moves3file.py

--- moves3file.py
+++ moves3file.py
@@ -1,1 +1,17 @@
+import boto3
 
+# Set the name of the S3 bucket and the file to move
+bucket_name = 'your_bucket_name'
+old_file_key = 'path/to/old/file.txt'
+new_file_key = 'path/to/new/file.txt'
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Copy the file to the new location
+s3.copy_object(Bucket=bucket_name, CopySource=bucket_name+'/'+old_file_key, Key=new_file_key)
+
+# Delete the file from the old location
+s3.delete_object(Bucket=bucket_name, Key=old_file_key)
+
+print('File moved successfully!')
