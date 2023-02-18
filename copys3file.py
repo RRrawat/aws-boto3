51671ef4ca05a6b7186c7853e9473a1a0e4a367b
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-18 22:23:14 +0530

    Update copys3file.py

--- copys3file.py
+++ copys3file.py
@@ -1,1 +1,13 @@
+import boto3
 
+# set up the S3 client
+s3 = boto3.client('s3')
+
+# set up the source and destination bucket and key names
+src_bucket = 'source-bucket'
+src_key = 'path/to/source/file'
+dst_bucket = 'destination-bucket'
+dst_key = 'path/to/destination/file'
+
+# copy the file
+s3.copy_object(Bucket=dst_bucket, CopySource={'Bucket': src_bucket, 'Key': src_key}, Key=dst_key)
