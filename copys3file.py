14b50c9fb2f6288c5f10dc52fff5e33d4b45b3e1
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-18 22:24:45 +0530

    Update copys3file.py

--- copys3file.py
+++ copys3file.py
@@ -13,3 +13,14 @@ dst_key = 'path/to/destination/file'
 
 # copy the file
 s3.copy_object(Bucket=dst_bucket, CopySource={'Bucket': src_bucket, 'Key': src_key}, Key=dst_key)
+
+
+"""
+In this code, you first set up a client for S3 using the boto3.client method. Then you define the source and destination bucket and key names. 
+Finally, you use the s3.copy_object method to copy the file, specifying the source bucket and key with the CopySource parameter, 
+and the destination bucket and key with the Bucket and Key parameters, respectively.
+
+
+
+
+"""
