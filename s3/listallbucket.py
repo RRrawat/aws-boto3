273862458d71e4f640b8a1ffd4ba1ede467411b6
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-20 10:20:22 +0530

    Update listallbucket.py

--- s3/listallbucket.py
+++ s3/listallbucket.py
@@ -20,3 +20,11 @@ for bucket in response['Buckets']:
     
     # Print the bucket details
     print(f'Bucket name: {bucket_name}\nCreation date: {creation_date}\nRegion: {location}\n')
+
+ """
+ This script uses the list_buckets() method to get a list of all the S3 buckets in the account. Then it loops through each bucket, gets its name, 
+ creation date, and region using the get_bucket_location() method, and prints the details to the console.
+
+Note that the get_bucket_location() method may return None for buckets located in the US East (N. Virginia) region, as this is the default region 
+and does not require a LocationConstraint parameter. In this case, the script will print us-east-1 as the region for the bucket.
+    """
