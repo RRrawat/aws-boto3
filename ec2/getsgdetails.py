0a160c79c6c5729c7d62a6ec7ffde5a96b23bbf4
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-22 11:00:55 +0530

    Update getsgdetails.py

--- ec2/getsgdetails.py
+++ ec2/getsgdetails.py
@@ -1,1 +1,33 @@
 #here is an example Python script that uses the Boto3 library to retrieve details of an Amazon Web Services (AWS) security group:
+import boto3
+
+# Replace with your own region
+region_name = 'us-west-2'
+
+# Replace with your own security group ID
+security_group_id = 'sg-0123456789abcdefg'
+
+# Create a session with Boto3
+session = boto3.session.Session(region_name=region_name)
+
+# Create an EC2 client object
+ec2 = session.client('ec2')
+
+# Use the describe_security_groups method to retrieve details of the security group
+response = ec2.describe_security_groups(GroupIds=[security_group_id])
+
+# Extract the relevant information from the response
+security_group = response['SecurityGroups'][0]
+
+# Print the details of the security group
+print(f"Security group ID: {security_group['GroupId']}")
+print(f"VPC ID: {security_group['VpcId']}")
+print(f"Description: {security_group['Description']}")
+print("Inbound rules:")
+for rule in security_group['IpPermissions']:
+    print(f"\t{rule['IpProtocol']} from {rule['FromPort']} to {rule['ToPort']}")
+    for ip_range in rule['IpRanges']:
+        print(f"\t\t{ip_range['CidrIp']}")
+print("Outbound rules:")
+for rule in security_group['IpPermissionsEgress']:
+    print(f"\t{rule['IpProtocol']} from {rule['FromPort']} to {rule['ToPort']} to {rule['IpRanges'][0]['CidrIp']}")
