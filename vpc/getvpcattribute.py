819e461b2863b3948f4c0f2dcdd98f817315148a
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-25 00:34:29 +0530

    Update getvpcattribute.py

--- vpc/getvpcattribute.py
+++ vpc/getvpcattribute.py
@@ -1,1 +1,3 @@
-
+"""
+Before we get started, make sure you have Boto3 installed on your machine, and that you have the necessary AWS credentials to access the VPC you want to interact with. Once that's done, here's a Python script that should do the trick:
+"""
