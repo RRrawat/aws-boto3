6b8dd1550965b97be4c7317dcbdb0e0eb67fd65b
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-26 09:07:00 -1000

    Update getip.py

--- ec2/getip.py
+++ ec2/getip.py
@@ -1,1 +1,4 @@
-
+"""
+we can use the describe_instances method from the boto3 library to get the IP addresses of all EC2 instances in your AWS account. 
+Here is an example Python code snippet that uses boto3 to achieve this:
+"""
