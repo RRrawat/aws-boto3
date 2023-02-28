c7b966e3c6672f49793cdac2f24dcd7fdc4ff005
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-28 12:10:32 +0530

    Update listmultipleservice.py

--- listmultipleservice.py
+++ listmultipleservice.py
@@ -2,13 +2,14 @@
 To list details of all running AWS services in an account using Python boto3, you can use a combination of several AWS service clients and API calls. 
 Here's an example script that demonstrates how to do this:
 """
-
 import boto3
 
 # create clients for each service you want to get information for
 ec2 = boto3.client('ec2')
 rds = boto3.client('rds')
 ecs = boto3.client('ecs')
+sqs = boto3.client('sqs')
+s3 = boto3.client('s3')
 
 # get a list of all running EC2 instances
 ec2_response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
@@ -49,6 +50,30 @@ for cluster in ecs_response['clusterArns']:
         print(f"ECS Container Instance Count: {c['registeredContainerInstancesCount']}")
         print(f"ECS Active Service Count: {c['activeServicesCount']}")
         print("")
+
+# get a list of all running SQS queues
+sqs_response = sqs.list_queues()
+
+# iterate over the queues and print their details
+for queue_url in sqs_response['QueueUrls']:
+    queue_name = queue_url.split('/')[-1]
+    queue_attributes = sqs.get_queue_attributes(QueueUrl=queue_url, AttributeNames=['All'])
+    print(f"SQS Queue Name: {queue_name}")
+    print(f"SQS Queue URL: {queue_url}")
+    print(f"SQS Queue Approximate Number of Messages: {queue_attributes['Attributes']['ApproximateNumberOfMessages']}")
+    print("")
+
+# get a list of all running S3 buckets
+s3_response = s3.list_buckets()
+
+# iterate over the buckets and print their details
+for bucket in s3_response['Buckets']:
+    bucket_name = bucket['Name']
+    bucket_location = s3.get_bucket_location(Bucket=bucket_name)['LocationConstraint']
+    print(f"S3 Bucket Name: {bucket_name}")
+    print(f"S3 Bucket Location: {bucket_location}")
+    print("")
+
 
 """
 This code will list details of all running EC2 instances, RDS instances, and ECS clusters in the default region for your AWS account, along with their 
