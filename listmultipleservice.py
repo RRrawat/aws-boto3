0fe38d9173b5f415b5d3b4671d0ca43903c8fbc0
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-02-28 11:41:11 +0530

    Update listmultipleservice.py

--- listmultipleservice.py
+++ listmultipleservice.py
@@ -1,1 +1,1 @@
-
+#You can use the boto3 library to list all running services in an AWS account by calling the describe_instances method of the EC2 client. Here's an example Python script that does this:
