2ca1056720cc1835430c6d6efe841ff00b8605e6
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-03-01 15:52:07 +0530

    Update getfile.py

--- sftptos3/getfile.py
+++ sftptos3/getfile.py
@@ -1,1 +1,34 @@
 #here's an example of how to write Python code to copy a file from SFTP to S3 
+
+import boto3
+import paramiko
+import os
+
+# Define the SFTP server details
+sftp_host = "sftp.example.com"
+sftp_port = 22
+sftp_username = "username"
+sftp_password = "password"
+sftp_remote_path = "/path/to/remote/file.txt"
+
+# Define the S3 bucket details
+s3_bucket = "my-s3-bucket"
+s3_prefix = "sftp-files/"
+
+# Connect to the SFTP server
+transport = paramiko.Transport((sftp_host, sftp_port))
+transport.connect(username=sftp_username, password=sftp_password)
+sftp = transport.open_sftp()
+
+# Download the file from SFTP
+local_path = "/tmp/file.txt"
+sftp.get(sftp_remote_path, local_path)
+
+# Upload the file to S3
+s3_client = boto3.client("s3")
+s3_key = os.path.join(s3_prefix, os.path.basename(local_path))
+s3_client.upload_file(local_path, s3_bucket, s3_key)
+
+# Close the SFTP connection
+sftp.close()
+transport.close()
