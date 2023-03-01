f9a5e07cd2422f8fc866db0b397e8d3376193e27
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-03-01 15:54:43 +0530

    Update getfile1.py

--- sftptos3/getfile1.py
+++ sftptos3/getfile1.py
@@ -1,1 +1,32 @@
 #Here's a better way to write Python code that copies a file from SFTP to S3 triggered by an EventBridge batch job:
+
+import paramiko
+import boto3
+import os
+
+# Set up SFTP connection
+hostname = "sftp.example.com"
+port = 22
+username = "myusername"
+password = "mypassword"
+remote_path = "/path/to/file.txt"
+local_path = "/tmp/file.txt"
+
+transport = paramiko.Transport((hostname, port))
+transport.connect(username=username, password=password)
+
+sftp = paramiko.SFTPClient.from_transport(transport)
+sftp.get(remote_path, local_path)
+
+# Set up S3 connection
+s3 = boto3.resource('s3')
+bucket_name = 'mybucket'
+key = 'file.txt'
+s3_path = os.path.join('s3://', bucket_name, key)
+
+# Upload file to S3
+s3.Bucket(bucket_name).upload_file(local_path, key)
+
+# Close connections
+sftp.close()
+transport.close()
