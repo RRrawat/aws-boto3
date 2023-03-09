75edd2fe5d5171bfd4eff6962eb596cd2bb82067
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-03-08 16:53:58 -1200

    Update stopinstance.py

--- ec2/stopinstance.py
+++ ec2/stopinstance.py
@@ -2,3 +2,12 @@
 To stop an EC2 instance using Python, you can use the Boto3 library which is the Amazon Web Services (AWS) SDK for Python. 
 
 """
+
+import boto3
+
+# Create an EC2 client
+ec2_client = boto3.client('ec2', aws_access_key_id='access_key', aws_secret_access_key='secret_key', region_name='us-east-1')
+
+# Stop the instance
+response = ec2_client.stop_instances(InstanceIds=['instance_id'])
+print('Instance stopped:', response['StoppingInstances'][0]['InstanceId'])
