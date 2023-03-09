4fe4df561be49496b6f7e34af0cc5d091bcf0a95
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-03-08 16:54:56 -1200

    Update stopinstance.py

--- ec2/stopinstance.py
+++ ec2/stopinstance.py
@@ -1,6 +1,5 @@
 """
 To stop an EC2 instance using Python, you can use the Boto3 library which is the Amazon Web Services (AWS) SDK for Python. 
-
 """
 
 #Import the Boto3 library and create an EC2 client using the AWS access key and secret key. Replace the access_key and secret_key with your own credentials.
