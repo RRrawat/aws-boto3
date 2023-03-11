ffda38900986282ae19a31c390bae3c395ac025b
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-03-11 04:23:45 -1200

    Update listmultipleservice.py

--- listmultipleservice.py
+++ listmultipleservice.py
@@ -1,8 +1,6 @@
 """
-
 To list details of all running AWS services in an account using Python boto3, you can use a combination of several AWS service clients and API calls. 
 Here's an example script that demonstrates how to do this:
-
 """
 import boto3
 
