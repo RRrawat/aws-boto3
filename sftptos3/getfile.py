03a829970adf1919cba494700410d16ce35cf3a5
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2023-03-11 04:22:34 -1200

    Update getfile.py

--- sftptos3/getfile.py
+++ sftptos3/getfile.py
@@ -11,28 +11,27 @@ sftp_password = "password"
 sftp_remote_path = "/path/to/remote/file.txt"
 
 # Define the S3 bucket details
-
 s3_bucket = "my-s3-bucket"
 s3_prefix = "sftp-files/"
 
-# Connect to the SFTP server
+#Connect to the SFTP server
 
 transport = paramiko.Transport((sftp_host, sftp_port))
 transport.connect(username=sftp_username, password=sftp_password)
 sftp = transport.open_sftp()
 
-# Download the file from SFTP
+#Download the file from SFTP
 
 local_path = "/tmp/file.txt"
 sftp.get(sftp_remote_path, local_path)
 
-# Upload the file to S3
+#Upload the file to S3
 
 s3_client = boto3.client("s3")
 s3_key = os.path.join(s3_prefix, os.path.basename(local_path))
 s3_client.upload_file(local_path, s3_bucket, s3_key)
 
-# Close the SFTP connection
+#Close the SFTP connection
 
 sftp.close()
 transport.close()
